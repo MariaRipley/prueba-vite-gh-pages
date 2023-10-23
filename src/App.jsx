947f6830422ee2df4b428479a3d7f4b29fb03323
MariaRipley <roblesb.maria@gmail.com>
2023-10-23 18:39:38 +0200

    Show Giphy Gifs using API and Postman

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-const nombre = "María";
+import { useState } from "react";
 
 function App() {
+  //Estado
+  const [valorInput, setValorInput] = useState("");
+  const [gifs, setGifs] = useState([]);
+
+  const onChange = (evento) => {
+    const valor = evento.target.value;
+    setValorInput(valor);
+  };
+
+  const getGifs = async (query) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=wyI9pq95jLjyZ2y6a3RToecTAkmYCK8n&q=${query}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data.data;
+  };
+
+  //Ciclo de vida
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const gifs = await getGifs(valorInput);
+    setGifs(gifs);
+  };
+
+  //JSX
   return (
     <div className="App">
-      <h1>¡Hola {nombre}!</h1>
+      <form onSubmit={onSubmit}>
+        <input value={valorInput} onChange={onChange} />
+      </form>
+      {gifs.map((gif) => (
+        <img key={gif.id} src={gif.images.original.url} alt="" />
+      ))}
     </div>
   );
 }
